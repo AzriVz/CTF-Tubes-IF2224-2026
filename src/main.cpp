3d362ff65d22d5c96929b8bad92e554d6b43bf85
Nathaniel Christian <nathaniel.christian@gmail.com>
2026-05-03 14:28:25 +0700

    feat: add runner for parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,75 @@
 #include "Lexer/Lexer.hpp"
+#include "Parser/Parser.hpp"
 #include <string>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
-        cerr << "Format: '/bin/arion-compiler /test/milestone-X/<input-file.txt>' atau bisa menggunakan 'make run'" << endl;
+        cerr << "Usage: ./bin/arion-compiler [--parse] <input-file.txt>" << endl;
+        cerr << "  Default: lexer only" << endl;
+        cerr << "  --parse: run lexer + parser and output parse tree" << endl;
+        cerr << "  Example: ./bin/arion-compiler test.txt" << endl;
+        cerr << "  Example: ./bin/arion-compiler --parse test.txt" << endl;
         return 1;
-    }    
-    string filename = argv[1]; 
+    }
+    
+    bool parseMode = false;
+    string filename;
+    
+    if(argc >= 3 && string(argv[1]) == "--parse") {
+        parseMode = true;
+        filename = argv[2];
+    } else {
+        filename = argv[1];
+    }
+    
     try {
-        Lexer lexer(filename);
-        Token token = lexer.getNextToken();        
-        while(token.type != TokenType::END_OF_FILE) {
-            string typeName = lexer.tokenTypeToString(token.type);
+        if(parseMode) {
+            // Parser mode
+            Lexer lexer(filename);
+            Parser parser(lexer);
+            ParseNode* tree = parser.parseProgram();
             
-            if(token.type == TokenType::IDENT || token.type == TokenType::INTCON || token.type == TokenType::REALCON) {
-                cout << typeName << " (" << token.value << ")" << endl;
+            // Print tree to stdout
+            tree->print(cout);
+            
+            // Save tree to output file
+            string outfile = filename;
+            size_t lastDot = outfile.rfind('.');
+            if(lastDot != string::npos) {
+                outfile = outfile.substr(0, lastDot) + "_parse.txt";
+            } else {
+                outfile = outfile + "_parse.txt";
             }
-            else if (token.type == TokenType::STRING || token.type == TokenType::CHARCON) {
-                cout << typeName << " ('" << token.value << "')" << endl;
+            ofstream out(outfile);
+            tree->print(out);
+            out.close();
+            cerr << "Parse tree saved to: " << outfile << endl;
+            
+            delete tree;
+        } else {
+            // Lexer mode (default)
+            Lexer lexer(filename);
+            Token token = lexer.getNextToken();        
+            while(token.type != TokenType::END_OF_FILE) {
+                string typeName = lexer.tokenTypeToString(token.type);
+                
+                if(token.type == TokenType::IDENT || token.type == TokenType::INTCON || token.type == TokenType::REALCON) {
+                    cout << typeName << " (" << token.value << ")" << endl;
+                }
+                else if (token.type == TokenType::STRING || token.type == TokenType::CHARCON) {
+                    cout << typeName << " ('" << token.value << "')" << endl;
+                }
+                else if (token.type == TokenType::UNKNOWN) {
+                    cout << typeName << " (" << token.value << ")" << endl;
+                }
+                else {
+                    cout << typeName << endl;
+                }            
+                token = lexer.getNextToken(); 
             }
-            else if (token.type == TokenType::UNKNOWN) {
-                cout << typeName << " (" << token.value << ")" << endl;
-            }
-            else {
-                cout << typeName << endl;
-            }            
-            token = lexer.getNextToken(); 
         }
     }
     catch (const exception& e) {
